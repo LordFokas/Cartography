70f6079d75386f8104f3e4b934fc64d5daabdb2f
LordFokas <lordfokas@gmail.com>
2021-03-09 22:33:26 +0000

    Initial setup (that refuses to work)

---
+++ LICENSE.md
@@ -0,0 +1,1 @@
+This repository is ©2021 LordFokas ("the author") under the [“Don’t Be a Jerk” non-commercial care-free license](https://github.com/LordFokas/DBaJ/blob/master/LICENSE.md).
---
+++ src/main/java/lordfokas/cartography/Cartography.java
@@ -0,0 +1,49 @@
+package lordfokas.cartography;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("cartography")
+public class Cartography {
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Cartography(){
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event){
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event){
+        // do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            // register a new block here
+            LOGGER.info("HELLO from Register Block");
+        }
+    }
+}
